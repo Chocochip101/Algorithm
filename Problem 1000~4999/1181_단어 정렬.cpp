ee3c42333e9706bf95f121fbd9d041eb97de5b81
user name <user e-mail>
2021-04-14 23:22:04 +0900

    BOJ

---
+++ Problem 1000~4999/1181_단어 정렬.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+vector<string> words;
+
+bool compare(string x, string y) {
+	if (x.size() == y.size()) return x < y;
+	return x.size() < y.size();
+}
+int main() {
+	int n;
+	cin >> n;
+	words.resize(n);
+	for (int i = 0; i < n; ++i)
+		cin >> words[i];
+	//정렬
+	sort(words.begin(), words.end(), compare);
+	//중복 제거
+	words.erase(unique(words.begin(), words.end()), words.end());
+
+	for (int i = 0; i < words.size(); ++i)
+		cout << words[i] << endl;
+	return 0;
+}
