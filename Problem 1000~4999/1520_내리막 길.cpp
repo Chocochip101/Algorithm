4229420c079dd022ade8f00a88bfcd332783db46
Kwoun Ki Ho <fingercut@naver.com>
2021-09-29 15:39:38 +0900

    1520.py

--- Problem 1000~4999/1520_내리막 길.cpp
+++ Problem 1000~4999/1520_내리막 길.cpp
@@ -1,43 +1,44 @@
 #include <bits/stdc++.h>
 #define endl "\n"
-#define ll long long
-#define INF 987654321
-#define MAX 101
-#define MOD 10007
+#define MAX  501
+#define int long long
 using namespace std;
 typedef pair<int, int> p;
+p Dir[4] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
 
-int n, m;
-int board[501][501];
-int cache[501][501];
-p Dir[4] = { {1, 0},{-1, 0} , {0, 1},{0, -1} };
+int m, n;
+int board[MAX][MAX];
+int cache[MAX][MAX];
 
-int solve(int x, int y) {
-	if (x == n - 1 && y == m - 1) return 1;
+int dfs(int x, int y) {
+	if (x == 0 && y == 0) return 1;
 
 	int& ret = cache[x][y];
 	if (ret != -1) return ret;
+
 	ret = 0;
 	for (int i = 0; i < 4; ++i) {
 		int nx = x + Dir[i].first;
 		int ny = y + Dir[i].second;
-		if (0 <= nx && nx < n && 0 <= ny && ny < m)
-			if (board[nx][ny] < board[x][y])
-				ret += solve(nx, ny);
+
+		if (0 <= nx && nx < m && 0 <= ny && ny < n) 
+			if(board[nx][ny] > board[x][y])
+				ret += dfs(nx, ny);
+		
 	}
 	return ret;
 }
-
-int main() {
-	ios_base::sync_with_stdio(0);
+signed main() {
+	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> n >> m;
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < m; ++j)
-			cin >> board[i][j];
 	memset(cache, -1, sizeof(cache));
 
-	cout << solve(0, 0) << endl;
+	cin >> m >> n;
+	for (int i = 0; i < m; ++i)
+		for (int j = 0; j < n; ++j)
+			cin >> board[i][j];
+
+	cout << dfs(m - 1, n - 1) << endl;
 	return 0;
 }
