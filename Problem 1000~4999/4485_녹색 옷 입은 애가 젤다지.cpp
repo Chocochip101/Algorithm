1252dde0fc6badacd4f8f070e5b087fa18538b0c
Kwoun Ki Ho <73146678+Chocochip101@users.noreply.github.com>
2021-10-21 23:09:01 +0900

    Update 4485_녹색 옷 입은 애가 젤다지.cpp

--- Problem 1000~4999/4485_녹색 옷 입은 애가 젤다지.cpp
+++ Problem 1000~4999/4485_녹색 옷 입은 애가 젤다지.cpp
@@ -1,3 +1,18 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+#define INF 987654321
+#define MAX 126
+#define MOD 1000000000
+#define int ll
+using namespace std;
+typedef pair<int, int> p;
+
+int n;
+int board[MAX][MAX]; 
+int dist[MAX][MAX];
+p Dir[4] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
+
 int bfs() {
 	for (int i = 0; i < n; ++i)
 		for (int j = 0; j < n; ++j)
@@ -16,7 +31,7 @@ int bfs() {
 			int nx = x + Dir[i].first;
 			int ny = y + Dir[i].second;
 			if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-				// ºñ¿ëÀÌ ´õ Å¬ °æ¿ì, update
+				// 비용이 더 클 경우, update
 				if (dist[nx][ny] > dist[x][y] + board[nx][ny]) {
 					dist[nx][ny] = dist[x][y] + board[nx][ny];
 					q.push({ nx, ny });
@@ -25,4 +40,19 @@ int bfs() {
 		}
 	}
 	return dist[n - 1][n - 1];
+}
+
+signed main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int tc = 1;
+	while (++tc) {
+		cin >> n;
+		if (n== 0) break;
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j)
+				cin >> board[i][j];
+		cout << "Problem " << tc - 1 << ": " << bfs() << endl;
+	}
+	return 0;
 }
