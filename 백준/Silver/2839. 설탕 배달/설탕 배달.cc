3bfb7af0bc4a764fd32dcdfcee627808bbfea703
Kwoun Ki Ho <73146678+Chocochip101@users.noreply.github.com>
2022-10-07 02:25:11 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define INF 987654321
+using namespace std;
+
+int n;
+
+int main() {
+	cin >> n;
+	vector <long long> dp(5001, INF);
+	dp[3] = 1, dp[5] = 1;
+	for (int i = 6; i < n + 1; ++i) {
+		dp[i] = min(dp[i - 3] + 1, dp[i - 5] + 1);
+	}
+	if (dp[n] >= INF) cout << -1 << "\n";
+	else cout << dp[n] << "\n";
+	return 0;
+}
