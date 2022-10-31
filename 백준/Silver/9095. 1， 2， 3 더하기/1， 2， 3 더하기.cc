427747c4a976f0488e3b7cf990e5d88002a8354e
Kwoun Ki Ho <73146678+Chocochip101@users.noreply.github.com>
2022-10-31 17:04:56 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+int solve(int n) {
+	if (n == 1) return 1;
+	if (n == 2) return 2;
+	if (n == 3)return 4;
+	return solve(n - 1) + solve(n - 2) + solve(n - 3);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	int tc; cin >> tc;
+	for (int i = 0; i < tc; ++i) {
+		int n; cin >> n;
+		cout << solve(n) << endl;
+	}
+
+	return 0;
+}
