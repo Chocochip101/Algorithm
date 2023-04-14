6da7d7ca96b20f48454e161452a0b3fc72d9a400
Kwoun Ki Ho <73146678+Chocochip101@users.noreply.github.com>
2023-04-14 14:52:02 +0900

    [level 2] Title: 주식가격, Time: 14.57 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,12 +1,15 @@
-from collections import deque
 def solution(prices):
-    answer = [0 for _ in range(len(prices))]
-    for i in range(len(prices)):
-        for j in range(i + 1, len(prices)):
-            if prices[i] <= prices[j]:
-                answer[i] += 1
-            else:
-                answer[i] += 1
-                break
-
+    answer = [0]*len(prices)
+    stack = []
+    for i, price in enumerate(prices):
+        #stack이 비었이면 false
+        while stack and price < prices[stack[-1]]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+    # for문 다 돌고 Stack에 남아있는 값들 pop
+    while stack:
+        j = stack.pop()
+        answer[j] = len(prices) - 1 - j
+    
     return answer
