11fd29453a0585b10d18fc4caf90f6325f4833a7
Kwoun Ki Ho <73146678+Chocochip101@users.noreply.github.com>
2023-04-28 03:23:48 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int angle) {
+        if(angle == 180){
+            return 4;
+        }else if(90 < angle && angle < 180){
+            return 3;
+        }else if(angle == 90){
+            return 2;
+        }
+        return 1;
+    }
+}
