9045a2aabf34dd13cc18f0285a939700aea27454
Kwoun Ki Ho <73146678+Chocochip101@users.noreply.github.com>
2023-04-28 20:00:30 +0900

    [Bronze I] Title: 평균, Time: 272 ms, Memory: 19340 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,23 +1,19 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();
-        List<Double> A = new ArrayList<>();
+        ArrayList<Integer> scores = new ArrayList<>();
 
-        for(int i = 0; i < N; ++i){
-            A.add(sc.nextDouble());
+        int total = 0;
+        for(int i = 0; i < N; ++ i){
+            scores.add(sc.nextInt());
+            total += scores.get(i);
         }
-        double m = Collections.max(A);
-
-        double res = 0;
-        for(int i = 0; i < N; ++i){
-            res += (A.get(i)/m * 100);
-        }
-        System.out.print(res / N);
+        
+        System.out.println(total * 100.0 / Collections.max(scores) / N);
     }
 }
