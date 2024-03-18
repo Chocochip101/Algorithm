a1afeaadef8586fbe5af55a38c678e52acbc9729
Kwoun Ki Ho <73146678+Chocochip101@users.noreply.github.com>
2024-03-18 15:39:14 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,12 +1,3 @@
 def solution(data, col, row_begin, row_end):
-    data.sort(key = lambda x : (x[col - 1], -x[0]))
-
     answer = 0
-    for i in range(row_begin, row_end + 1):
-        answer = answer ^ S(data[i - 1], i)
     return answer
-def S(data, i):
-    ret = 0
-    for d in data:
-        ret += d % i
-    return ret
